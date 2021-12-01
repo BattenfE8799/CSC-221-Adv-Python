88e3f4d5691e4ff1b5c1b7089fe6fb122fa92ff1
Elizabeth Battenfield <71232235+BattenfE8799@users.noreply.github.com>
2021-12-01 18:12:35 -0500

    worked on it

--- items_try2.py
+++ items_try2.py
@@ -8,7 +8,10 @@ class Items:
         self.__description = description
         self.__value = value
         self.__weight = weight
-        self.__carryable = carryable
+        
+        #flag commands for inheritance
+        self._canGet = True #default to gettable/pickup
+        self._canDrop = True #the drop id to see if items can be dropped
         
         
     
@@ -57,96 +60,25 @@ class Weapons(UsableItems):
         super().__init__(name, description, value, weight, carryable, durability)
         self._damage = damage
 
+class Doors(Items):
+    """ doors class that is for doors and locked doors"""
+    def __init__(self, name, description, value, weight, carryable, locked):
+        super().__init__(name, description, value, weight, carryable)
+        self._locked = locked
         
-class Weapon:
-    def __init__(self):
-        raise NotImplementedError("Do not create raw Weapon objects.")
-
-    def __str__(self):
-        return self.name
-
-
-class Knife(Weapon):
-    def __init__(self):
-        self.name = "Knife"
-        self.description = "A pocket knife, good for self-defence."
-        self.damage = 5
-        self.value = 1
-
-
-class NailGun(Weapon):
-    def __init__(self):
-        self.name = "Nail Gun"
-        self.description = "An old abandoned nail gun, surprisingly servicable."
-        self.damage = 10
-        self.value = 15
-
-
-class Chainsaw(Weapon):
-    def __init__(self):
-        self.name = "Chainsaw"
-        self.description = "A well-maintained chainsaw, might be more effective"\
-            "than your current weapon."
-        self.damage = 20
-        self.value = 25
-
-'''
-class (Weapon):
-    def __init__(self):
-        self.name = ""
-        self.description = ""
-        self.damage = 30
-        self.value = 50
-'''
-class LasGun(Weapon):
-    def __init__(self):
-        self.name = "LasGun"
-        self.description = "Does a ton of damage, no recoil and lookin' cool doing it."
-        self.damage = 70
-        self.value = 100
-
-class Consumable:
-    def __init__(self):
-        raise NotImplementedError("Do not create raw Consumable objects.")
-
-    def __str__(self):
-        return "{} (+{} HP)".format(self.name, self.healing_value)
-
-
-class Bandaid(Consumable):
-    def __init__(self):
-        self.name = "Bandaid"
-        self.healing_value = 10
-        self.value = 20
-
-
-class HealingStim(Consumable):
-    def __init__(self):
-        self.name = "Healing Stim"
-        self.healing_value = 50
-        self.value = 60
-
-class NanoHeal(Consumable):
-    def __init__(self):
-        self.name = "Healing Stim"
-        self.healing_value = 100
-        self.value = 85
-
-
-class PuzzlePiece:
-    def __init__(self):
-        raise NotImplementedError("Do not create raw Puzzle Piece objects.")
-
-    def __str__(self):
-        return "{}".format(self.name)
-
-
-class KeyCard(PuzzlePiece):
-    def __init__(self):
-        self.name = "Key"
-        self.value = 10
-
-class Key(PuzzlePiece):
-    def __init__(self):
-        self.name = "Key"
-        self.value = 10
+    def set_locked(self, locked):
+        self._locked = locked
+    
+    def get_locked(self):
+        return self._locked
+    
+    def unlock(self, locked, key):
+        if locked == 0:
+            return True
+        elif locked != 0:
+            if key == True:
+                self.set_locked(0)
+                return True
+        else:
+            print("You have no key for that door. The door remains locked.")
+        
